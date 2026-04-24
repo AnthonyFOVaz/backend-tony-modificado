575f077200696fb0afda9a7ea37378467b321f95
Enzo Lima <olucca104@gmail.com>
2026-04-23 22:45:09 -0300

    feat: corrige EscolaController.

--- src/main/java/br/com/ucsal/controller/EscolaController.java
+++ src/main/java/br/com/ucsal/controller/EscolaController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/requisicoes")
+@RequestMapping("/api/escolas")
 @RequiredArgsConstructor
 public class EscolaController {
     private final EscolaService escolaService;
